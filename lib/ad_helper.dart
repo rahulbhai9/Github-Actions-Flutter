2bd747b2a9ee8ea47d57c85c42f8e9a9f82f8312
rahulbhai9 <79494222+rahulbhai9@users.noreply.github.com>
2022-04-21 18:22:59 +0530

    Test benner ads with test adunits

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -1,10 +1,24 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'dart:io';
 
 class AdHelper {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-1770077062185460/3851157049';
+      return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/2934735716';
     } else {
@@ -14,7 +28,7 @@ class AdHelper {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-1770077062185460/3826272161';
+      return 'ca-app-pub-3940256099942544/7049598008';
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/3964253750';
     } else {
@@ -24,7 +38,7 @@ class AdHelper {
 
   static String get rewardedAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-1770077062185460/3288050267';
+      return 'ca-app-pub-3940256099942544/8673189370';
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/7552160883';
     } else {
